a810e81fa18b2038a8e8a343e6e028a403a0eedf
Reese6 <kurnosov1998@list.ru>
2020-08-03 11:39:03 +0300

    master: work on registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    # previous login view
+    # url(r'^login/$', views.user_login, name='login'),
+
+    # login / logout urls
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
 ]
